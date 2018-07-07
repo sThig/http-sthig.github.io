eb3e8de511c6a909edfd88a6dba7d204ad1a6ee9
sThig <scott.thigpen@gmail.com>
2018-07-06 19:58:53 -0500

    fixed issue with random word! thanks stacko

--- assets/javascripts/helper.js
+++ assets/javascripts/helper.js
@@ -1,16 +1,23 @@
 //run random word on index
 
-window.onload =
-  function refreshWord() {
-    const frontPageSwap = document.querySelector('front-page-text-swap');
-    setInterval(function randomWord() {
-      const words = ['designer', 'developer', 'illustrator', 'animator'];
-      const randomGenerator = Math.floor(Math.random() * words.length);
-      document.getElementById('textswap').innerHTML = words[randomGenerator];
-    }, 1000);
-  };
+// window.onload =
+//   function refreshWord() {
+//     const frontPageSwap = document.querySelector('front-page-text-swap');
+//     setInterval(function randomWord() {
+//       const words = ['designer', 'developer', 'illustrator', 'animator'];
+//       const randomGenerator = Math.floor(Math.random() * words.length);
+//       document.getElementById('textswap').innerHTML = words[randomGenerator];
+//     }, 1000);
+//   };
 
-
+if (window.location.pathname === '/index.html') {
+  const words = ['Engineer', 'Developer', 'Illustrator', 'Cyclist', 'Artist'];
+  const getRandomWord = () => words[Math.floor(Math.random() * words.length)];
+  const textswap = document.getElementById('textswap');
+  setInterval(() => {
+    textswap.textContent = getRandomWord();
+  }, 1000);
+}
 
 
 
